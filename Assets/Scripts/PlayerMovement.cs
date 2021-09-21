4719e4e873f6cd935c7ee6a0d669ccd54d9c6851
Lucas Redding <lre61@uclive.ac.nz>
2021-09-21 23:41:49 +1200

    Player controller

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D Controller;
+
+    public float moveSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Movement
+        Controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+        jump = false;
+    }
+}
