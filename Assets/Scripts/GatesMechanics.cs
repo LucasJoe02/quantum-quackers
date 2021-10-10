6bf9015f8c8dcb49e7533d1b9e0d23985d93def5
Akmal Zahidi Bin Zahari <azb12@uclive.ac.nz>
2021-10-10 15:52:30 +1300

    More bug fixes

--- Assets/Scripts/GatesMechanics.cs
+++ Assets/Scripts/GatesMechanics.cs
@@ -24,7 +24,7 @@ public class GatesMechanics : MonoBehaviour
         //Debug.Log("duck2 = " + charge2.chargeLevel);
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Duck1")
         {
